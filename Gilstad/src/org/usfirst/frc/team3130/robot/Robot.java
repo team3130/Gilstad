5ffb852da0998665533bc95d66ad8c6ccf6182b3
Ashley H <ashleyholton2@hotmail.com>
2016-12-15 19:13:36 -0600

    instantiation (reprise)
    same as last update, forgot to save the file (lol)

--- Gilstad/src/org/usfirst/frc/team3130/robot/Robot.java
+++ Gilstad/src/org/usfirst/frc/team3130/robot/Robot.java
@@ -5,8 +5,11 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 import org.usfirst.frc.team3130.robot.commands.ExampleCommand;
 import org.usfirst.frc.team3130.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team3130.robot.subsystems.Vroom;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -30,6 +33,7 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		OI.GetInstance();
+		Vroom.GetInstance();
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
